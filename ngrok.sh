25d34acd32dc5013be479adb2b7263d5c151a436
waduk79 <140233689+waduk79@users.noreply.github.com>
2023-07-25 20:52:21 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,6 +8,6 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip  > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 
